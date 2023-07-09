e8ac5fc31da26dcf91024d1ad9946b2f50840134
Li Xin Rui <li.xinrui3@gmail.com>
2023-07-09 07:17:46 -0400

    added circle poggerino

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,22 @@ mod vec3;
 use crate::{
     color::write_color,
     ray::Ray,
-    vec3::{unit_vector, Color, Point3, Vec3},
+    vec3::{dot, unit_vector, Color, Point3, Vec3},
 };
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - *center;
+    let a = dot(r.direction(), r.direction());
+    let b = 2.0 * dot(oc, r.direction());
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction: Vec3 = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
