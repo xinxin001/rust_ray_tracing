ede9500da37ab0257bfef010a80ef3b935bd2814
Li Xin Rui <li.xinrui3@gmail.com>
2023-07-11 08:51:44 -0400

    added shading with surface normals

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,23 @@ use crate::{
     vec3::{dot, unit_vector, Color, Point3, Vec3},
 };
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - *center;
     let a = dot(r.direction(), r.direction());
     let b = 2.0 * dot(oc, r.direction());
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    return (-b - discriminant.sqrt()) / (2.0 * a);
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
     }
     let unit_direction: Vec3 = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
