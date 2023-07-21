707fe6c28b137a0c5083a91907e1eb3d305a0aa4
Li Xin Rui <li.xinrui3@gmail.com>
2023-07-20 22:07:18 -0400

    math helpers

--- src/rtweekend.rs
+++ src/rtweekend.rs
@@ -1,8 +1,18 @@
-use std::{cell::RefCell, f64, rc::Rc};
+use std::f64;
+
+use rand::Rng;
 
 pub const INFINITY: f64 = f64::INFINITY;
 pub const PI: f64 = f64::consts::PI;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
+}
+
+pub fn random_double() -> f64 {
+    rand::thread_rng().gen_range(0.0..1.0)
+}
+
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+    rand::thread_rng().gen_range(min..max)
 }
