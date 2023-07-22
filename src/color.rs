ca9165bddd3969d2e1cefffa680429e52836a893
Li Xin Rui <li.xinrui3@gmail.com>
2023-07-22 01:28:41 -0400

    added gamma correction for color

--- src/color.rs
+++ src/color.rs
@@ -7,8 +7,8 @@ pub fn write_color(
     samples_per_pixel: u32,
 ) -> std::io::Result<()> {
     let scale = 1.0 / samples_per_pixel as f64;
-    let r = (256.0 * clamp(pixel_color[0] * scale, 0.0, 0.999)) as i32;
-    let g = (256.0 * clamp(pixel_color[1] * scale, 0.0, 0.999)) as i32;
-    let b = (256.0 * clamp(pixel_color[2] * scale, 0.0, 0.999)) as i32;
+    let r = (256.0 * clamp((pixel_color[0] * scale).sqrt(), 0.0, 0.999)) as i32;
+    let g = (256.0 * clamp((pixel_color[1] * scale).sqrt(), 0.0, 0.999)) as i32;
+    let b = (256.0 * clamp((pixel_color[2] * scale).sqrt(), 0.0, 0.999)) as i32;
     writeln!(out, "{} {} {}", r, g, b)
 }
