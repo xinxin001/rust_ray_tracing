37c34a1983dc8dc7bde5399e0fb251c3214d739b
Li Xin Rui <li.xinrui3@gmail.com>
2024-10-04 16:16:26 -0400

    added some reformat

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,11 @@
 use crate::{
+    color::write_color,
+    hittable::{HitRecord, Hittable},
+    hittable_list::HittableList,
     ray::Ray,
-    vec3::{Point3, Vec3},
+    rtweekend::random_double,
+    rtweekend::INFINITY,
+    vec3::{unit_vector, Color, Point3, Vec3},
 };
 
 pub struct Camera {
@@ -8,10 +13,23 @@ pub struct Camera {
     lower_left_corner: Point3,
     horizontal: Vec3,
     vertical: Vec3,
+    aspect_ratio: f64,
+    pub image_width: usize,
+    pub image_height: usize,
+    samples_per_pixel: u32,
+    max_depth: u32,
 }
 
 impl Camera {
     pub fn new() -> Self {
+        // Image
+        let aspect_ratio = 1.;
+        let image_width: usize = 400;
+        let mut image_height: usize = (image_width as f64 / aspect_ratio) as usize;
+        image_height = if image_height < 1 { 1 } else { image_height };
+        let samples_per_pixel = 10;
+        let max_depth = 10;
+
         let aspect_ratio = 16.0 / 9.0;
         let focal_length = 1.0;
 
@@ -29,6 +47,11 @@ impl Camera {
             lower_left_corner,
             horizontal,
             vertical,
+            aspect_ratio,
+            image_width,
+            image_height,
+            samples_per_pixel,
+            max_depth,
         }
     }
 
@@ -37,5 +60,44 @@ impl Camera {
             self.origin,
             self.lower_left_corner + self.horizontal * u + self.vertical * v - self.origin,
         );
+    }
+
+    fn ray_color(&self, r: &Ray, world: &dyn Hittable, depth: u32) -> Color {
+        let mut rec = HitRecord::default();
+        if depth <= 0 {
+            return Color::new(0.0, 0.0, 0.0);
+        }
+        if world.hit(r, 0.001, INFINITY, &mut rec) {
+            let mut scattered = Ray::default();
+            let mut attenuation = Color::new(0.0, 0.0, 0.0);
+            if rec
+                .mat_ptr
+                .as_ref()
+                .unwrap()
+                .scatter(r, &rec, &mut attenuation, &mut scattered)
+            {
+                return attenuation * self.ray_color(&scattered, world, depth - 1);
+            }
+            return Color::new(0.0, 0.0, 0.0);
+        }
+        let unit_direction: Vec3 = unit_vector(r.direction());
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    }
+
+    pub fn render(&self, world: &HittableList) {
+        for j in (0..self.image_height).rev() {
+            eprintln!("Scanlines remaining: {}", j);
+            for i in 0..self.image_width {
+                let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+                for _ in 0..self.samples_per_pixel {
+                    let u = (i as f64 + random_double()) / (self.image_width - 1) as f64;
+                    let v = (j as f64 + random_double()) / (self.image_height - 1) as f64;
+                    let r = self.get_ray(u, v);
+                    pixel_color += self.ray_color(&r, world, self.max_depth);
+                }
+                let _ = write_color(&mut std::io::stdout(), pixel_color, self.samples_per_pixel);
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,15 @@ mod vec3;
 
 use std::rc::Rc;
 
-use hittable::{HitRecord, Hittable};
 use material::Dielectric;
-use rtweekend::INFINITY;
-use vec3::random_in_hemisphere;
 
 use crate::{
     camera::Camera,
-    color::write_color,
     hittable_list::HittableList,
     material::{Lambertian, Metal},
     ray::Ray,
-    rtweekend::random_double,
     sphere::Sphere,
-    vec3::{dot, unit_vector, Color, Point3, Vec3},
+    vec3::{dot, Color, Point3},
 };
 
 fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
@@ -36,29 +31,6 @@ fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
         return -1.0;
     }
     return (-half_b - discriminant.sqrt()) / a;
-}
-
-fn ray_color(r: &Ray, world: &dyn Hittable, depth: u32) -> Color {
-    let mut rec = HitRecord::default();
-    if depth <= 0 {
-        return Color::new(0.0, 0.0, 0.0);
-    }
-    if world.hit(r, 0.001, INFINITY, &mut rec) {
-        let mut scattered = Ray::default();
-        let mut attenuation = Color::new(0.0, 0.0, 0.0);
-        if rec
-            .mat_ptr
-            .as_ref()
-            .unwrap()
-            .scatter(r, &rec, &mut attenuation, &mut scattered)
-        {
-            return attenuation * ray_color(&scattered, world, depth - 1);
-        }
-        return Color::new(0.0, 0.0, 0.0);
-    }
-    let unit_direction: Vec3 = unit_vector(r.direction());
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
 fn main() -> std::io::Result<()> {
@@ -96,32 +68,13 @@ fn main() -> std::io::Result<()> {
         material_right.clone(),
     )));
 
-    // Image
-    let aspect_ratio = 16. / 9.;
-    let image_width: usize = 400;
-    let mut image_height: usize = (image_width as f64 / aspect_ratio) as usize;
-    image_height = if image_height < 1 { 1 } else { image_height };
-    let samples_per_pixel = 50;
-    let max_depth = 50;
-
     // Camera
     let camera = Camera::new();
 
-    println!("P3\n{} {}\n255", image_width, image_height);
+    println!("P3\n{} {}\n255", camera.image_width, camera.image_height);
+    camera.render(&world);
 
-    for j in (0..image_height).rev() {
-        eprintln!("Scanlines remaining: {}", j);
-        for i in 0..image_width {
-            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
-            for _ in 0..samples_per_pixel {
-                let u = (i as f64 + random_double()) / (image_width - 1) as f64;
-                let v = (j as f64 + random_double()) / (image_height - 1) as f64;
-                let r = camera.get_ray(u, v);
-                pixel_color += ray_color(&r, &world, max_depth);
-            }
-            write_color(&mut std::io::stdout(), pixel_color, samples_per_pixel)?;
-        }
-    }
+    // REFACTOR INTO A RENDER METHOD
     eprint!("Done.");
     Ok(())
 }
